5fcc0b31a82ae0b981d59a8bef9950f5b0082844
diegodiezrevuelta-star <diegodiezrevuelta@gmail.com>
2026-09-16 10:22:06 +0200

    CNN4.py

--- CNN4.py
+++ CNN4.py
@@ -346,7 +346,7 @@ print(f"Correlación de Pearson: {r_pearson:.4f} (p-value: {p_value:.4e})")
 print(f"Coeficiente de Determinación (R²): {r2:.4f}")
 
 # Representación del rendimiento
-# Calculo recta de ajuste
+# Calculo de la recta de ajuste
 m, b = np.polyfit(y_true, y_pred, 1)
 
 plt.figure(figsize=(8, 6))
